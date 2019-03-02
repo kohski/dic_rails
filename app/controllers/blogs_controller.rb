a3e1d948774988071a763a7b75212b7b3863770a
kosukekimura <kimkim5364@gmail.com>
2019-03-02 23:57:42 +0000

    proceed to the lesson 17 of rails entry

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,14 @@
 class BlogsController < ApplicationController
   def index
   end
+  
+  def new
+    @blog = Blog.new
+  end
+  
+  def create
+    Blog.create(title: params[:blog][:title],content: params[:blog][:content])
+    redirect_to new_blog
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'stocks/index'
-  get 'stocks', to: 'stocks#index'
+  # get 'stocks/index'
+  # get 'stocks', to: 'stocks#index'
 
-  get 'blogs/index'
-  get 'blogs', to: 'blogs#index'
+  # get 'blogs/index'
+  # get 'blogs', to: 'blogs#index'
+
+  resources :blogs
 
 end
